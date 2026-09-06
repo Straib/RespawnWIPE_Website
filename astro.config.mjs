4b2e348ff3ae5ba1a94bc9afc6244178ae48d354
Robin Pösselt <r.poesselt@googlemail.com>
2026-09-06 17:31:04 +0200

    add SSR and docker file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,17 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Respawn-WIPE.com',
   base: '/',
   trailingSlash: 'always',
-  integrations: [icon()]
+  output: 'server',
+  integrations: [icon()],
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
